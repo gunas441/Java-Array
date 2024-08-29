c6791e022fe64913f9410bf176a90a3377037d46
Guna <64432253+gunas441@users.noreply.github.com>
2024-08-29 23:05:59 +0530

    Create ArrayLastElament.java

---
+++ ArrayLastElament.java
@@ -0,0 +1,32 @@
+/*Array find last element*/
+
+public class ArrayLastElament
+{
+    public static void main(String[] args) {
+        int [] arr={2,5,7,8,5};
+        findLastElament(arr);
+    }
+
+    public static void findLastElament(int[] arr){
+        int n = arr.length;
+        int temps=0;
+        int largest=0;
+        int[] arr2 = new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    temps=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temps;
+                }
+            }
+           arr2[i]=arr[i];
+        }
+        for (int i : arr2) {
+            if (i > largest) {
+                largest = i;
+            }
+        }
+        System.out.println("Largest = "+largest);
+    }
+}
